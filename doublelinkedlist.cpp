d05a9ed9365e1c02b09160e86530226e1cf9d66d
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-30 21:42:51 +0700

    void addNode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -5,8 +5,143 @@ using namespace std;
 
 class Node
 {
-    public:
+public:
         int nMhs;
         Node* next;
         Node* prev;
+};
+
+class DoubleLinkedList
+{   
+private:
+        Node* START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+        
+        // Step 1: Allocate memory for a new node
+        Node* newNode = new Node();
+
+        // Step 2:Assign value to the data fields
+        newNode->nMhs = nim;
+
+        // Step 3: Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->nMhs)
+        {
+            if (START != NULL && nim == START->nMhs)
+            {
+                cout << "\nDuplicate numbers not allowed." << endl;
+                return;
+            }
+            // Step 4: newNode.next = START
+            newNode->next = START;
+
+            // Step 5: START.prev = newNode (if START is exists)
+            if (START != NULL)
+                START->prev = newNode;
+            
+            // Step 6: newnode.prev = NULL
+            newNode->prev = NULL;
+
+            // Step 7: START = newNode
+            START = newNode;
+            return;
+        }
+
+        //insert inbetween node
+        // Step 8: Locate position for insertion
+        Node* current = START;
+        while (current->next != NULL && current->next->nMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->nMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed." << endl;
+            return;
+        }
+
+        // Step 9: Insert between current and current->next
+        newNode->next = current->next; // Step 9a: newNode.next = current.next
+        newNode->prev = current;      // Step 9b: newNode.prev = current
+
+        // insert last node
+        if (current->next != NULL)
+            current->next->prev = newNode; // Step 9c: current.next.prev = newNode
+
+        current->next = newNode; // Step 9d: current.next = newNode
+    }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty." << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node* current = START;
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->nMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
+        // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b: START.prev = NULL
+        }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: If current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev; 
+        }
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << "deleted" << endl;
+    }
+
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty." << endl;
+            return;
+        }
+
+        cout << "\nRecords in the list are: " << endl;
+        Node* current = START;
+        while (current != NULL)
+        {
+            cout << current->nMhs << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+    
 };
